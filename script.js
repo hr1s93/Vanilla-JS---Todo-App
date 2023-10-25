a110c4d6cea33e8a0a37e4af703e440de7607e26
hr1s93 <142612927+hr1s93@users.noreply.github.com>
2023-10-25 09:58:32 +0200

    Update script.js
    Added localStorage to save the todos after the page reloads

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const addBtn = document.getElementById("add");
-addBtn.addEventListener("click", addTodo);
-
 function addTodo() {
   const inputValue = document.getElementById("input").value;
-  const todos = document.getElementById("lists");
-  const task = document.createElement("p");
-  const delBtn = document.createElement("button");
 
   if (inputValue === "") {
     return;
   }
 
+  const todos = document.getElementById("lists");
+  const task = document.createElement("p");
+  const delBtn = document.createElement("button");
+
   delBtn.innerHTML = "X";
   delBtn.id = "deleteButton";
   task.innerHTML = inputValue;
   task.classList = "incomplete";
+
+  saveTodo(inputValue);
 
   todos.appendChild(task);
   task.appendChild(delBtn);
@@ -33,8 +33,68 @@ function addTodo() {
   });
 }
 
+function saveTodo(todoText) {
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  todos.push(todoText);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 function deleteTodo() {
   const item = this.parentNode;
   const todos = document.getElementById("lists");
   todos.removeChild(item);
+
+  const todoText = item.textContent;
+  removeTodo(todoText);
 }
+
+function removeTodo(todoText) {
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  todos = todos.filter((todo) => todo !== todoText);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  if (savedTodos) {
+    const todos = document.getElementById("lists");
+
+    for (const todoText of savedTodos) {
+      const task = document.createElement("p");
+      const delBtn = document.createElement("button");
+
+      delBtn.innerHTML = "X";
+      delBtn.id = "deleteButton";
+      task.innerHTML = todoText;
+      task.classList = "incomplete";
+
+      todos.appendChild(task);
+      task.appendChild(delBtn);
+      task.style.listStyle = "none";
+
+      delBtn.addEventListener("click", deleteTodo);
+
+      task.addEventListener("click", () => {
+        if (task.classList.contains("incomplete")) {
+          task.classList.remove("incomplete");
+          task.classList.add("completed");
+        } else {
+          task.classList.remove("completed");
+          task.classList.add("incomplete");
+        }
+      });
+    }
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  e.key === "Enter" ? addTodo() : "";
+});
+
+const addBtn = document.getElementById("add");
+addBtn.addEventListener("click", addTodo);
