63d83614f44716f30ea6c41ac54897ee08485cec
Hristiyan Hristov <142612927+hr1s93@users.noreply.github.com>
2023-11-02 13:18:24 +0100

    Update script.js
    fixed the input field to be empty after entering a task

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function addTodo() {
   const inputValue = document.getElementById("input").value;
-
   if (inputValue === "") {
     return;
   }
@@ -13,6 +12,8 @@ function addTodo() {
   delBtn.id = "deleteButton";
   task.innerHTML = inputValue;
   task.classList = "incomplete";
+
+  saveTodo(inputValue);
 
   todos.appendChild(task);
   task.appendChild(delBtn);
@@ -28,71 +29,72 @@ function addTodo() {
       task.classList.remove("completed");
       task.classList.add("incomplete");
     }
-    saveTasks();
   });
+  input.value = "";
+}
 
-  saveTasks();
+function saveTodo(todoText) {
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  todos.push(todoText);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function deleteTodo() {
   const item = this.parentNode;
   const todos = document.getElementById("lists");
   todos.removeChild(item);
-  saveTasks();
+
+  const todoText = item.textContent;
+  removeTodo(todoText);
 }
 
-function saveTasks() {
-  const tasks = document.querySelectorAll("#lists p");
-  const taskList = [];
-  tasks.forEach((task) => {
-    taskList.push({
-      text: task.textContent,
-      completed: task.classList.contains("completed"),
-    });
-  });
-  localStorage.setItem("tasks", JSON.stringify(taskList));
+function removeTodo(todoText) {
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  todos = todos.filter((todo) => todo !== todoText);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function loadTasks() {
-  const savedTasks = JSON.parse(localStorage.getItem("tasks"));
-  if (savedTasks) {
-    savedTasks.forEach((task) => {
-      const todos = document.getElementById("lists");
-      const newTask = document.createElement("p");
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  if (savedTodos) {
+    const todos = document.getElementById("lists");
+
+    for (const todoText of savedTodos) {
+      const task = document.createElement("p");
       const delBtn = document.createElement("button");
 
       delBtn.innerHTML = "X";
       delBtn.id = "deleteButton";
-      newTask.innerHTML = task.text;
-      newTask.classList = task.completed ? "completed" : "incomplete";
+      task.innerHTML = todoText;
+      task.classList = "incomplete";
 
-      todos.appendChild(newTask);
-      newTask.appendChild(delBtn);
-      newTask.style.listStyle = "none";
+      todos.appendChild(task);
+      task.appendChild(delBtn);
+      task.style.listStyle = "none";
 
       delBtn.addEventListener("click", deleteTodo);
 
-      newTask.addEventListener("click", () => {
-        if (newTask.classList.contains("incomplete")) {
-          newTask.classList.remove("incomplete");
-          newTask.classList.add("completed");
+      task.addEventListener("click", () => {
+        if (task.classList.contains("incomplete")) {
+          task.classList.remove("incomplete");
+          task.classList.add("completed");
         } else {
-          newTask.classList.remove("completed");
-          newTask.classList.add("incomplete");
+          task.classList.remove("completed");
+          task.classList.add("incomplete");
         }
-        saveTasks();
       });
-    });
+    }
   }
-}
+});
 
 window.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    addTodo();
-  }
+  e.key === "Enter" ? addTodo() : "";
 });
 
 const addBtn = document.getElementById("add");
 addBtn.addEventListener("click", addTodo);
-
-loadTasks();
